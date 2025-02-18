9ea626a03d73a8f0d7317a2c76d3932ecebed2e3
Adhish Aggarwal <aggarwaladhish@ymail.com>
2025-02-18 16:29:48 +0530

    adding lsof to install

--- rocky_linux/run.sh
+++ rocky_linux/run.sh
@@ -10,7 +10,7 @@ sudo dnf makecache
 # Enable the EPEL repository (often needed for extra packages on Rocky Linux)
 sudo dnf install -y epel-release
 
-sudo dnf install -y git vim wget net-tools curl  postgresql-devel tmux
+sudo dnf install -y git vim wget net-tools curl  postgresql-devel tmux lsof
 # Install Tilix (a tiling terminal emulator)
 # Note: Tilix may not be packaged for Rocky Linux. If not, you may need to build it or find an alternative.
 sudo dnf install -y tilix
